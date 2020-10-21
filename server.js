39af1944f641489d6884ca3393a9e89cb2d301ae
iromashko <iromashko@me.com>
2020-10-21 07:32:27 +0300

    part 1

--- server.js
+++ server.js
@@ -1,6 +1,44 @@
 const express = require('express');
+const path = require('path');
+const http = require('http');
+const socketio = require('socket.io');
+const formatMessage = require('./utils/messages');
+const { userJoin, getCurrentUser } = require('./utils/users.js');
+
 const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
 
-const PORT = 3000 || process.env.PORT;
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.listen(PORT, () => console.log(`Server running at ${PORT}`));
+const botName = 'Chatcord Bot';
+
+io.on('connection', (socket) => {
+  socket.on('joinRoom', ({ username, room }) => {
+    socket.emit('message', formatMessage(botName, 'Welcome to Chatcord!'));
+
+    const user = userJoin(socket.id, username, room);
+
+    socket.join(user.room);
+
+    socket.broadcast
+      .to(user.room)
+      .emit(
+        'message',
+        formatMessage(botName, `${user.username} has joined the chat`)
+      );
+  });
+
+  socket.on('disconnect', () => {
+    io.emit('message', formatMessage(botName, 'A user has left the chat'));
+  });
+
+  socket.on('chatMessage', (msg) => {
+    const user = getCurrentUser(socket.id);
+    io.to(user.room).emit('message', formatMessage(user.username, msg));
+  });
+});
+
+const PORT = process.env.PORT || 3000;
+
+server.listen(PORT, () => console.log(`Server running at ${PORT}`));
